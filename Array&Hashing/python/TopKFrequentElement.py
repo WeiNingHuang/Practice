57b1167d7d6420f0a4f52036abb94d77f72238b0
Hwnl <louis840404@gmail.com>
2022-07-29 14:02:36 +0800

    update

--- Array&Hashing/python/TopKFrequentElement.py
+++ Array&Hashing/python/TopKFrequentElement.py
@@ -14,26 +14,30 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pass
-#%%
-nums = [8,8,8,6,6,6,5]
+        freq = [ [] for i in range(len(nums) + 1) ] 
+
+        count = {}
+
+        for i in nums:
+            count[i] = 1+count.get(i, 0)
+
+        for key, value in count.items():
+            freq[value].append(key)
+
+        res = []
+
+        for i in range(len(nums) ,0, -1):
+            for n in freq[i]:
+                res.append(n)
+
+                if len(res) == k:
+                    return res
+
+
+nums = [1,1,1,2,2,3]
 k = 2
 
-freq = [ [] for i in range(len(nums) + 1) ] 
+s = Solution()
+print(s.topKFrequent( nums, k ))
 
-count = {}
 
-for i in nums:
-    count[i] = 1+count.get(i, 0)
-
-for k, v in count.items():
-    freq[v].append(k)
-
-res = []
-
-for i in range(len(nums) - 1 ,0, -1):
-    for n in freq[i]:
-        res.append(n)
-        if len(res) ==k:
-            print(res)
-            break
