32580d7182f433a54aa1d297ad127cb40f92137c
Hwnl <louis840404@gmail.com>
2022-08-01 14:35:41 +0800

    Update

--- Array&Hashing/python/ProductofArrayExceptSelf.py
+++ Array&Hashing/python/ProductofArrayExceptSelf.py
@@ -16,3 +16,14 @@ class Solution:
 #%%
 nums = [-1,1,0,-3,3]
 
+res = [1] * len(nums)
+
+prefix = 1
+for i in range(len(nums)):
+    res[i] = prefix
+    prefix *= nums[i]
+
+postfix = 1
+for i in range(len(nums) - 1, -1, -1):
+    res[i] *= postfix
+    postfix *= nums[i]
