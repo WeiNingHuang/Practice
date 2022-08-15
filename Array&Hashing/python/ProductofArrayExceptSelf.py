9b83770ce11dcb28294e722025a19eb277ce164b
Hwnl <louis840404@gmail.com>
2022-08-15 16:34:17 +0800

    Update

--- Array&Hashing/python/ProductofArrayExceptSelf.py
+++ Array&Hashing/python/ProductofArrayExceptSelf.py
@@ -13,17 +13,3 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         pass
 
-#%%
-nums = [-1,1,0,-3,3]
-
-res = [1] * len(nums)
-
-prefix = 1
-for i in range(len(nums)):
-    res[i] = prefix
-    prefix *= nums[i]
-
-postfix = 1
-for i in range(len(nums) - 1, -1, -1):
-    res[i] *= postfix
-    postfix *= nums[i]
