86eedfc8a681d029febf4ad0b4a35640a60abf31
Hwnl <louis840404@gmail.com>
2022-08-29 18:20:41 +0800

    Update

--- Array&Hashing/python/ValidSudoku.py
+++ Array&Hashing/python/ValidSudoku.py
@@ -22,43 +22,31 @@ Input: board =
 Output: true
 """
 ##
-board = [["5","3",".",".","7",".",".",".","."]
+from collections import defaultdict
+board = [["5","5",".",".","7",".",".",".","."]
         ,["6",".",".","1","9","5",".",".","."]
         ,[".","9","8",".",".",".",".","6","."]
         ,["8",".",".",".","6",".",".",".","3"]
         ,["7",".",".","8",".","3",".",".","1"]
-        ,["7",".",".",".","2",".",".",".","6"]
-        ,[".","6",".",".",".",".","2","8","."]
+        ,["9",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","5","8","."]
         ,[".",".",".","4","1","9",".",".","5"]
         ,[".",".",".",".","8",".",".","7","9"]]
 
-for r in board:
-    # row = {}
-    # for i in r:
-    #     if i == ".":
-    #         continue
-    #     if i not in row:
-    #         row[i] = 1
-    #     else:
-    #         print("row false")
-    col = {}
-    for j in range(len(board)):
-        for c in r[j]:
-            if c == ".":
-                continue
+row = defaultdict(list)
+sqr = defaultdict(list)
+col = defaultdict(list)
 
-            if c not in col:
-                col[c] = 1
-            else:
-                print("col false")
-# [row[0] for row in board]
-##
-
-for r in board:
-    for j in range(len(board)):
-        for c in r[j]:
-            print(c)
-
-r=board[0]
-
-[row[0] for row in board]
+for r in range(len(board)):
+    for c in range(len(board)):
+        num = board[r][c]
+        if num =='.':
+            continue 
+        
+        if (num in row[r] or num in col[c] or num in sqr[(r//3, c//3)]):
+            print(False)
+        else:
+            row[r].append(num)
+            col[c].append(num)
+            sqr[(r//3, c//3)].append(num)
+                
