d75442fc9c22418d3e469efbb6725e3d5cdc4b8a
WeiNingHuang <louis840404@gmail.com>
2023-03-20 09:22:29 +0800

    update

--- LeetCode75/Lvl1/Day1_Prefix Sum/724.Find Pivot Index/C++/FindPivotIndex.cpp
+++ LeetCode75/Lvl1/Day1_Prefix Sum/724.Find Pivot Index/C++/FindPivotIndex.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <vector>
+#include <numeric>
 using namespace std;
 
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
+        
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        
+
         for(vector<int>::iterator it = nums.begin(); it != nums.end(); it ++){
 
         }
