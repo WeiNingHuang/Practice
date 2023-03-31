6f1ac09798674b0b1d41ff983e80f326e2c3c53a
WeiNingHuang <louis840404@gmail.com>
2023-03-31 15:47:45 +0800

    update

---
+++ LeetCode Study_Data Structure/Data Structre1/Day1_Array/53. Maximum Subarray/MaximumSubarray.py
@@ -0,0 +1,17 @@
+##
+from typing import List
+import math
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        total = 0
+        
+
+        
+
+##
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+s = Solution()
+
+s.maxSubArray(nums)
